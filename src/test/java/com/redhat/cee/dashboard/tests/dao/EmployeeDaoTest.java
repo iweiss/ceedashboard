9b1a50aa77f9e34b4e59640b16d40857c4a9b8a4
Ingo Weiss <ingo@redhat.com>
2014-11-25 21:08:54 +0000

    * Arquillian setup * Arquillian tests working

--- src/test/java/com/redhat/cee/dashboard/tests/dao/EmployeeDaoTest.java
+++ src/test/java/com/redhat/cee/dashboard/tests/dao/EmployeeDaoTest.java
@@ -1,9 +1,9 @@
 package com.redhat.cee.dashboard.tests.dao;
 
-import javax.inject.Inject;
 import com.redhat.cee.dashboard.dao.EmployeeDao;
 import com.redhat.cee.dashboard.domain.Employee;
-import static junit.framework.TestCase.assertNotNull;
+import com.redhat.cee.dashboard.domain.Engineer;
+import com.redhat.cee.dashboard.util.LogFactory;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -11,6 +11,13 @@ import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.TestCase.assertNotNull;
 
 
 /**
@@ -23,12 +30,17 @@ public class EmployeeDaoTest
     @Inject
     EmployeeDao dao;
 
+    @Inject
+    Logger LOG;
+
     @Deployment
     public static WebArchive createDeployment()
     {
-        WebArchive deployment = ShrinkWrap.create(WebArchive.class)
-                                    .addClasses(EmployeeDao.class, Employee.class)
-                                    .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        WebArchive deployment = ShrinkWrap.create(WebArchive.class, "ceedashboard-test.war")
+                .addClasses(EmployeeDao.class, LogFactory.class)
+                .addPackage(Employee.class.getPackage())
+                .addAsResource("META-INF/persistence.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
 
         return deployment;
     }
@@ -36,6 +48,26 @@ public class EmployeeDaoTest
     @Test
     public void injectContainer()
     {
+        LOG.info("Testing DAO injection...");
         assertNotNull(dao);
+    }
+
+    @Test
+    public void daoPersist()
+    {
+        LOG.info("Testing DAO persist()");
+
+        Engineer employee = new Engineer();
+
+        employee.setEmail("asd@asd.com");
+        employee.setName("ASD ASD");
+        employee.setOffice("FAB");
+        employee.setPosition("TSE");
+        ArrayList<String> sbrs = new ArrayList<>();
+        sbrs.add("sbr-networking");
+        sbrs.add("sbr-storage");
+        employee.setSbrs(sbrs);
+
+        assertTrue(dao.save(employee));
     }
 }
